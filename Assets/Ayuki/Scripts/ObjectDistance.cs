d34152f2a8b14b8412a5e1cda773ca1df97d6114
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-03-04 13:40:47 +0900

    地球と隕石の距離の表示地球の表示

---
+++ Assets/Ayuki/Scripts/ObjectDistance.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObjectDistance : MonoBehaviour
+{
+    public GameObject gameObject;
+    public GameObject gameObject2;
+    public TextMeshProUGUI text;
+
+    void Update()
+    {
+        float dis = Vector3.Distance(gameObject.transform.position, gameObject2.transform.position);
+        Debug.Log("‹——£ : " + dis);
+
+        
+        text.GetComponent<TextMeshProUGUI>().text = dis.ToString();
+        //text.text = string.Format(" ",dis);
+
+    }
+}
