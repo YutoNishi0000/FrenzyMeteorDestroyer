12c13663f142295ce70b955dc5539c3c7a0b37de
YutoNishi0000 <yuto0324n@gmail.com>
2023-03-04 16:39:29 +0900

    とりあえず

--- Assets/Nishimura/Scripts/UFOController.cs
+++ Assets/Nishimura/Scripts/UFOController.cs
@@ -8,31 +8,51 @@ public class UFOController : MonoBehaviour
 {
     [SerializeField] private float speed;   //動くスピード
     private float randAngle;      //動く方向がx軸から何度離れているか
-    private Vector3 direction;
+    private Vector2 directionVec;
     private Rigidbody2D rb;
+    private bool direction;
+    [SerializeField] private readonly float changeTime;    //何秒後に方向転換するか
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         randAngle = Random.Range(0, 360);
-        direction = GetDirection(randAngle);
+        directionVec = GetDirectionVec(randAngle);
+        direction = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity += (Vector2)direction * Time.deltaTime * speed;
+        rb.velocity += directionVec.normalized * Time.deltaTime * speed;
     }
 
-    private Vector3 GetDirection(float angle)
+    private Vector2 GetDirectionVec(float angle)
     {
         float rad = angle * Mathf.Deg2Rad;
 
         float x = Mathf.Cos(rad);
         float y = Mathf.Sin(rad);
-        float z = 0;
 
-        return new Vector3(x, y, z);
+        return new Vector2(x, y);
+    }
+
+    private int GetDirection(bool flag)
+    {
+        if (flag)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    private bool InverseFlag(bool flag)
+    {
+        flag = !flag;
+        return flag;
     }
 }
