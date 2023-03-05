6a6c1a60b60a06d829aa7e15782ab6504c9a218e
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-03-05 15:23:54 +0900

    HPにあわせてテキストの変更

--- Assets/Ayuki/Scripts/result.cs
+++ Assets/Ayuki/Scripts/result.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class result : MonoBehaviour
 {
@@ -11,7 +12,23 @@ public class result : MonoBehaviour
     private AudioClip clip1; //音源データ1
 
     [SerializeField]
-    private AudioClip clip2; 
+    private AudioClip clip2;
+
+    [SerializeField]
+    private Sprite spriteearth1;
+
+    [SerializeField]
+    private Sprite spriteearth2;
+
+    [SerializeField]
+    private Sprite spriteearth3;
+
+    [SerializeField]
+    private GameObject earth;
+
+    [SerializeField]
+    private GameObject gameObject;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +36,27 @@ public class result : MonoBehaviour
         {
             if(MeteoriteController.lustHP==3)
             {
+                gameObject.GetComponent<Text>().text = "おめでとう!!地球は消えてしまったよ!!";
+                earth.SetActive(false);
                 source.clip = clip1;
             }
             else if(MeteoriteController.lustHP == 2)
             {
+                gameObject.GetComponent<Text>().text = "ナイス!!地球はほぼ壊滅したよ!!";
+                earth.GetComponent<SpriteRenderer>().sprite = spriteearth2;
                 source.clip = clip2;
             }
             else if (MeteoriteController.lustHP == 1)
             {
+                gameObject.GetComponent<Text>().text = "やったね!!生命は絶滅したよ!!";
+                earth.GetComponent<SpriteRenderer>().sprite = spriteearth3;
                 source.clip = clip2;
             }
                 
         }
         else{
             source.clip = clip2;
+            gameObject.GetComponent<Text>().text ="残念!!地球は無傷だ!!";
         }
          //再生したいclipを指定して
         source.Play();
