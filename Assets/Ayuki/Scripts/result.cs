f1969572f089d1a2a8b66911c03f4885870ebc98
2122075-PC\2122075 <s2122075@cco.kanagawa-it.ac.jp>
2023-03-05 10:43:18 +0900

    fontとリザルトの設定

--- Assets/Ayuki/Scripts/result.cs
+++ Assets/Ayuki/Scripts/result.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class result : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource source; //スピーカー・CDプレイヤー
+
+    [SerializeField]
+    private AudioClip clip1; //音源データ1
+
+    [SerializeField]
+    private AudioClip clip2; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        source.clip = clip1; //再生したいclipを指定して
+        source.Play();
     }
 
     // Update is called once per frame
