4d7b8ae92dae70f2f6b20f27ebb52d4833582933
YutoNishi0000 <yuto0324n@gmail.com>
2023-03-05 13:25:17 +0900

    シーン遷移確認できました

--- Assets/Nishimura/Scripts/SceneTransion.cs
+++ Assets/Nishimura/Scripts/SceneTransion.cs
@@ -1,19 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 //シーン遷移のためのクラス
 public class SceneTransion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NextScene(string sceneName)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(sceneName);
     }
 }
